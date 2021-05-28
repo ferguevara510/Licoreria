17fe937c4ba89c7fd6d4c3930399b234e810d547
Fer Guevara <kafera.g10@gmail.com>
2021-05-27 21:57:41 -0500

    Base de datos
    Clase para conectar ha base de datos

---
+++ src/modelo/util/SingleConnection.java
@@ -0,0 +1,27 @@
+package modelo.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SingleConnection {
+    
+    private SingleConnection(){}
+    
+    public static Connection getInstance(){
+        Connection coneccon = null;
+        try{
+            if(coneccon==null){
+                String bd = "Licoreria";
+                String login ="root";
+                String password ="";
+                String url="jdbc:mysql://localhost/" +bd;
+                Class.forName("com.mysql.jdbc.Driver");
+                coneccon=DriverManager.getConnection(url,login,password);
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        return coneccon;
+    }
+    
+}
