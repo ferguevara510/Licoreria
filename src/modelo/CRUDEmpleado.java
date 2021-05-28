4997f3368c195a10f14f32479b7d0eaf01a6783d
Nadia Bravo <nait0197@live.com.mx>
2021-05-27 22:24:34 -0500

    Caso de uso Registrar Empleado
    Se integra la parte del CU registrar empleado

---
+++ src/modelo/CRUDEmpleado.java
@@ -0,0 +1,66 @@
+package modelo;
+
+import modelo.util.SingleConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.Connection;
+
+public class CRUDEmpleado {
+    private final Connection con;
+    
+    public CRUDEmpleado() {
+        con = SingleConnection.getInstance();
+    }
+    
+     public void nuevoEmpleado(String numTrabajador, String nombre, String apellidos, String contraseña,Boolean tipo){
+        try {
+            PreparedStatement pstm;
+            pstm = con.prepareStatement("insert into empleado(numTrabajador,nombre,apellidos,contraseña,tipo) values(?,?,?,?,?)");
+                pstm.setString(1,numTrabajador);
+                pstm.setString(2,nombre);
+                pstm.setString(3,apellidos);
+                pstm.setString(4,contraseña);
+                pstm.setBoolean(5,tipo);
+                pstm.execute();
+                pstm.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }finally{
+            try {
+                if(!con.isClosed()){
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(CRUDEmpleado.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+     
+    public void editarEmpleado(String numT, Empleado empleado){
+        try{
+            PreparedStatement query; 
+            query = con.prepareStatement("UPDATE empleado SET nombre=?, apellidos= ?, contraseña= ?, tipo = ? WHERE numTrabajador= ?");
+            query.setString(1, empleado.getNombre());
+            query.setString(2, empleado.getApellidos());
+            query.setString(3, empleado.getContraseña());
+            query.setBoolean(4, empleado.isGerente());
+            query.setString(5, empleado.getNumTrabajador());
+                query.executeUpdate();
+                query.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }finally{
+            try {
+                if(!con.isClosed()){
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(CRUDEmpleado.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+}
---
+++ src/modelo/Empleado.java
@@ -0,0 +1,98 @@
+package modelo;
+
+import modelo.util.SingleConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Empleado {
+    //INSERT INTO empleado (numTrabajador, nombre, apellidos, contraseña, tipo) VALUES ('000', 'Administrator', 'Synthesis', 'delunoal4', 1);
+    private String numTrabajador;
+    private String nombre;
+    private String apellidos;
+    private String contraseña;
+    private String tipo;   //1= gerente, 0= trabajador
+    
+    public Empleado(){}
+    
+    public Empleado(String numTrabajador, String nombre, String apellidos, String contraseña, String tipo){
+        this.numTrabajador = numTrabajador;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.contraseña = contraseña;
+        this.tipo = tipo;
+    }
+
+    public void setNumTrabajador(String numTrabajador) {
+        this.numTrabajador = numTrabajador;
+    }
+    public String getNumTrabajador() {
+        return numTrabajador;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public String isTipo() {
+        return tipo;
+    }
+
+    @Override
+    public String toString(){
+        return "Numero de empleado: "+numTrabajador+"\nNombre: "+nombre+"\nApellidos: "+apellidos+"\nTipo: "+tipo;
+    }
+    
+    public Empleado obtenerEmpleado(String numTrabajador){
+        Connection connection = SingleConnection.getInstance();
+        Empleado empleado = null;
+        try {
+            PreparedStatement query = connection.prepareStatement("select * from empleado where numTrabajador = ?");
+            query.setString(1, numTrabajador);
+            
+            ResultSet result = query.executeQuery();
+            if(result.next()){
+                empleado = new Empleado();
+                empleado.setNumTrabajador(result.getString("numTrabajador"));
+                empleado.setTipo(Empleado.obtenerTipoEmpleado(result.getBoolean("tipo")));
+            }
+            result.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Empleado.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return empleado;
+    }
+    
+    public static String obtenerTipoEmpleado(Boolean tipo){
+        return tipo?"Gerente":"Trabajador";
+    }
+    
+    public Boolean isGerente(){
+        return this.tipo.equals("Gerente");
+    }
+    
+}
