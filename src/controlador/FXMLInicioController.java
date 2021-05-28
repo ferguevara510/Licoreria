95f698c7d4783998951efd82fa3797905838de41
Fer Guevara <kafera.g10@gmail.com>
2021-05-27 21:37:08 -0500

    Inicio sesion
    se genera el proyecto y se crea el inicio de sesión con vista, controlador y modelo

---
+++ src/controlador/FXMLInicioController.java
@@ -0,0 +1,100 @@
+package controlador;
+
+import modelo.util.SingleConnection;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import modelo.Empleado;
+
+public class FXMLInicioController implements Initializable{
+    Connection con = SingleConnection.getInstance();
+    ResultSet rs;
+    @FXML
+    private TextField usuarioTf;
+    @FXML
+    private PasswordField contraseñaPf;
+
+    @FXML
+    private void iniciarSesion(MouseEvent event) throws SQLException, IOException{
+       
+       if (camposVacios()) {
+            // allow user to decide between yes and no
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Hay campos vacios", ButtonType.OK);
+            // clicking X also means no
+            ButtonType result = alert.showAndWait().orElse(ButtonType.OK);
+            if (ButtonType.OK.equals(result)) {}
+        } else {
+            if(verificarUsuario()==1){
+                if(verificarContraseña()==1){
+                    Empleado empleado = new Empleado();
+                    empleado = empleado.obtenerEmpleado(usuarioTf.getText());
+                    System.getProperties().put("Empleado", empleado.isTipo());
+                    Parent root; 
+                    root = FXMLLoader.load(getClass().getClassLoader().getResource("vista/Principal.fxml"));
+                    Stage stage = new Stage();
+                    stage.setTitle(" Principal ");
+                    stage.setScene(new Scene(root, 650, 600));
+                    stage.show();
+                    stage.setResizable(false);
+                    stage.getIcons().add(new Image("/vista/imagenes/vintage.png"));
+                    ((Node)(event.getSource())).getScene().getWindow().hide(); 
+                }else{
+                    Alert alert = new Alert(Alert.AlertType.WARNING, " Contraseña incorrecta ", ButtonType.OK);
+                    ButtonType result = alert.showAndWait().orElse(ButtonType.OK);
+                    if (ButtonType.OK.equals(result)) {}
+                }
+            }else{
+                Alert alert = new Alert(Alert.AlertType.WARNING, " Usuario incorrecto ", ButtonType.OK);
+                ButtonType result = alert.showAndWait().orElse(ButtonType.OK);
+                if (ButtonType.OK.equals(result)) {}
+            }
+        }  
+    }
+    
+    private int verificarUsuario() throws SQLException{
+        int v=0;
+        rs = con.createStatement().executeQuery("select * from empleado where numTrabajador='"+usuarioTf.getText()+"'");
+        if(rs.next()){
+            v=1;
+        }else{
+            v=0;
+        }
+        return v;
+    }
+    
+    private int verificarContraseña() throws SQLException{
+        int v=0;
+        rs = con.createStatement().executeQuery("select * from empleado where numTrabajador='"+usuarioTf.getText()+"' and contraseña='"+contraseñaPf.getText()+"'");
+        if(rs.next()){
+            v=1;
+        }else{
+            v=0;
+        }
+        return v;
+    }
+    
+    private boolean camposVacios() {
+        return (usuarioTf.getText().isEmpty() || contraseñaPf.getText().isEmpty());
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+    
+}
---
+++ src/licoreria/Licoreria.java
@@ -0,0 +1,27 @@
+package licoreria;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Licoreria extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/vista/IniciarSesion.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("IniciarSesion");
+        stage.show();
+        stage.setResizable(false);
+        stage.getIcons().add(new Image("/vista/imagenes/vintage.png"));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
